d1e70dbd04c0c5c85c65fce9e707e95381ccb236
Steph <steph.ananth@gmail.com>
2021-03-26 12:56:27 -0400

    base app

---
+++ CopyCat/CopyCat/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  CopyCat
+//
+//  Created by Steph Ananth on 3/26/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let viewController = UIViewController()
+        let navigationController = UINavigationController()
+        navigationController.pushViewController(viewController, animated: true)
+        
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+        
+        return window != nil
+    }
+}
