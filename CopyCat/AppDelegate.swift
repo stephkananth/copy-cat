e1dd7e7d7e1b9e22713656fc5ab24fb368c196d8
Steph <steph.ananth@gmail.com>
2021-03-26 13:37:46 -0400

    autocorrected code

--- CopyCat/AppDelegate.swift
+++ CopyCat/AppDelegate.swift
@@ -10,16 +10,16 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
-    
+
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         let viewController = UIViewController()
         let navigationController = UINavigationController()
         navigationController.pushViewController(viewController, animated: true)
-        
+
         window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
-        
+
         return window != nil
     }
 }
